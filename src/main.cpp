c3fd7375f9230d4c373e4cd9128e7ea963399ccc
Yuuki Tsuchida <tsuchida_yuki_gn@cyberagent.co.jp>
2016-09-05 10:02:28 +0900

    initialize

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,14 @@
 
 #include <iostream>
 
-int main(int argc, char const* argv[])
+int main()
 {
   toml::bool_value value(false);
 
   std::cout << value.is<bool>() << std::endl;
   std::cout << value.is<int>() << std::endl;
 
-  toml::array<toml::table> table_array;
+  toml::array table_array;
   toml::table table; 
 
   table_array.add(table);
